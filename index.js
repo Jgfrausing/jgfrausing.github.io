d149521fb6d4cabe2b3f9e6b92834770640b6ddd
Jonatan Frausing <jof@lunar.app>
2022-12-14 12:20:41 +0100

    d13

--- index.js
+++ index.js
@@ -156,6 +156,10 @@ dict['d8'] = content([
      tag("p", "tekst og melodi: Jonatan"),
      img("d13.jpeg")
  ])
+ dict['d14'] = content([
+    tag("I aften skal vi have..."),
+    '<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="788.54" height="443" type="text/html" src="https://www.youtube.com/embed/toZW65rksYY?autoplay=1&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0"></iframe>'
+ ])
 
  
 function content(elems) {
